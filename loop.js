5e00583269b8b5767de2802230ad63fbec374c2c
smkisvarday <susannahkisvarday@hotmail.com>
2021-11-17 19:28:52 -0500

    commit1

---
+++ loop.js
@@ -0,0 +1,11 @@
+for(var i = 0; i <101; i++) {
+    console.log(i)
+}
+
+for(var i = 0; i <101; i++) {
+if (i % 2 === 0) {
+    console.log(i + " is even.")
+} else {
+  console.log(i + " is odd.")
+}
+}
